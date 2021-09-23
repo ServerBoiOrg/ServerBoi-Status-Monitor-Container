b7d635f12b065a225977c413a701b1c73b1d8886
awlsring <mattcanemail@gmail.com>
2021-09-22 21:42:34 -0700

    Fixing issue panicing getting a2s info

--- status-monitor/application_info.go
+++ status-monitor/application_info.go
@@ -19,29 +19,7 @@ type ApplicationInformation struct {
 func getApplicationInformation() *ApplicationInformation {
 	switch config.QueryType {
 	case "a2s":
-		// Put all this in A2S Query method
-		a2sInfo, a2sPlayers, err := a2sQuery()
-		if err != nil {
-			log.Printf("Error getting a2s info: %v", err)
-		}
-		var playerInfo []*Player
-		if a2sPlayers != nil {
-			for _, player := range a2sPlayers.Players {
-				newPlayer := &Player{
-					Name:     player.Name,
-					Duration: player.Duration,
-				}
-				playerInfo = append(playerInfo, newPlayer)
-			}
-		}
-		return &ApplicationInformation{
-			CurrentPlayers:    int(a2sInfo.Players),
-			MaxPlayers:        int(a2sInfo.MaxPlayers),
-			Map:               a2sInfo.Map,
-			PasswordProtected: a2sInfo.Visibility,
-			VAC:               a2sInfo.VAC,
-			Players:           playerInfo,
-		}
+		return a2sInfo()
 	default:
 		log.Printf("No query method specified.")
 		return &ApplicationInformation{}
@@ -56,12 +34,10 @@ func a2sQuery() (info *a2s.ServerInfo, players *a2s.PlayerInfo, err error) {
 		defer client.Close()
 		log.Printf("Querying a2s info.")
 		info, err = client.QueryInfo()
-		if err != nil {
-			log.Printf("Error querying info: %v", err)
-		}
 		players, err = client.QueryPlayer()
 		if err != nil {
-			log.Printf("Error querying players: %v", err)
+			log.Printf("Error querying info: %v", err)
+			return info, players, err
 		}
 		client.Close()
 		return info, players, nil
@@ -69,4 +45,32 @@ func a2sQuery() (info *a2s.ServerInfo, players *a2s.PlayerInfo, err error) {
 		log.Printf("Error creating client. Error: %v", err)
 		return info, players, err
 	}
+}
+
+func a2sInfo() *ApplicationInformation {
+	appInfo := &ApplicationInformation{}
+	info, players, err := a2sQuery()
+	if err != nil {
+		log.Printf("Error getting info: %v", err)
+	} else {
+		if info != nil {
+			appInfo.CurrentPlayers = int(info.Players)
+			appInfo.Map = info.Map
+			appInfo.PasswordProtected = info.Visibility
+			appInfo.MaxPlayers = int(info.MaxPlayers)
+			appInfo.VAC = info.VAC
+		}
+		if players != nil {
+			var playerInfo []*Player
+			for _, player := range players.Players {
+				newPlayer := &Player{
+					Name:     player.Name,
+					Duration: player.Duration,
+				}
+				playerInfo = append(playerInfo, newPlayer)
+			}
+			appInfo.Players = playerInfo
+		}
+	}
+	return appInfo
 }
